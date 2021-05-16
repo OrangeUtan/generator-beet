b0c8ea45662b3478a1da4f981f3c60d4116435c9
Oran9eUtan <Oran9eUtan@gmail.com>
2021-05-16 21:14:40 +0200

    feat: Generating beet configuration files and Lincense

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,38 +1,130 @@
 "use strict";
 const Generator = require("yeoman-generator");
-const chalk = require("chalk");
-const yosay = require("yosay");
+
+const defaultPackFormat = 7;
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the striking ${chalk.red("generator-beet")} generator!`)
-    );
-
-    const prompts = [
+  async prompting() {
+    this.answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Description"
+      },
+      {
+        type: "input",
+        name: "author",
+        message: "Your name",
+        default: this.user.git.name()
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Your email (optional)",
+        default: this.user.git.email()
+      },
       {
         type: "confirm",
-        name: "someAnswer",
-        message: "Would you like to enable this option?",
+        name: "generateResourcepack",
+        message: "Generate resourcepack?",
+        default: true
+      },
+      {
+        type: "confirm",
+        name: "generateDatapack",
+        message: "Generate datapack?",
         default: true
       }
-    ];
+    ]);
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
+    if (this.answers.generateResourcepack || this.answers.generateDatapack) {
+      const { packFormat } = await this.prompt([
+        {
+          type: "input",
+          name: "packFormat",
+          message: "Pack format",
+          default: defaultPackFormat
+        }
+      ]);
+      this.answers.packFormat = packFormat;
+    }
+
+    this.composeWith(require.resolve("generator-license"), {
+      name: this.answers.author,
+      email: this.answers.email,
+      defaultLicense: "MIT"
     });
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath("dummyfile.txt"),
-      this.destinationPath("dummyfile.txt")
+    this.fs.writeJSON(
+      this.destinationPath("beet.json"),
+      this._createBeetConfig()
+    );
+
+    this.fs.writeJSON(
+      this.destinationPath("beet-release.json"),
+      this._createBeetReleaseConfig()
     );
   }
 
-  install() {
-    this.installDependencies();
+  _createBeetConfig() {
+    const config = {
+      name: this.answers.name,
+      author: this.answers.author,
+      description: this.answers.description,
+      output: "build"
+    };
+
+    // Add resourcepack
+    if (this.answers.generateResourcepack) {
+      config.resource_pack = {
+        load: ["resourcepack"],
+        pack_format: this.answers.packFormat
+      };
+    }
+
+    // Add datapack
+    if (this.answers.generateDatapack) {
+      config.data_pack = {
+        load: ["datapack"],
+        pack_format: this.answers.packFormat
+      };
+    }
+
+    return config;
+  }
+
+  _createBeetReleaseConfig() {
+    const config = {
+      extend: ["beet.json"],
+      version: "0.0.0",
+      pipeline: ["beet.contrib.minify_json"],
+      output: "dist"
+    };
+
+    // Add resourcepack
+    if (this.answers.generateResourcepack) {
+      config.resource_pack = {
+        zipped: true,
+        name: `${this.answers.name}-resourcepack`
+      };
+    }
+
+    // Add datapack
+    if (this.answers.generateDatapack) {
+      config.data_pack = {
+        zipped: true,
+        name: `${this.answers.name}-datapack`
+      };
+    }
+
+    return config;
   }
 };

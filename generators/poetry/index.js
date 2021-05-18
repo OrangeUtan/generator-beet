a62026c0bdf30cc251ebd7b258958a8880b5916b
Oran9eUtan <Oran9eUtan@gmail.com>
2021-05-18 15:06:38 +0200

    feat: Added 'githubRelease' prompt

--- generators/poetry/index.js
+++ generators/poetry/index.js
@@ -1,5 +1,4 @@
 "use strict";
-const { default: chalk } = require("chalk");
 const Generator = require("yeoman-generator");
 
 module.exports = class extends Generator {
@@ -30,21 +29,64 @@ module.exports = class extends Generator {
             description: "Author",
             require: true
         });
+
+        this.option("githubReleases", {
+            type: String,
+            description: "Author",
+            require: true
+        });
+
+        this.props = {
+            name: this.options.name,
+            version: this.options.version,
+            description: this.options.description,
+            author: this.options.author,
+            git: this.options.git
+        };
+
+        if (!this.props.git) {
+            this.props.githubReleases = false;
+        }
+    }
+
+    async prompting() {
+        await this._promptAndUpdateProps([
+            {
+                type: "confirm",
+                name: "githubReleases",
+                message: "Automatically create Github releases?",
+                default: true,
+                when: this.props.githubReleases === undefined
+            }
+        ]);
+    }
+
+    async _promptAndUpdateProps(prompts) {
+        this.props = Object.assign({}, this.props, await this.prompt(prompts));
     }
 
     writing() {
         this.fs.copyTpl(
-            [this.templatePath(), this.templatePath(".github")],
+            [
+                this.templatePath("poetry.toml"),
+                this.templatePath("pyproject.toml"),
+                this.templatePath(".pre-commit-config.yaml")
+            ],
             this.destinationPath(),
-            {
-                ...this.options
-            }
+            { ...this.props }
         );
+
+        if (this.props.githubReleases) {
+            this.fs.copyTpl([this.templatePath(".github")], this.destinationPath(".github"), {
+                ...this.props
+            });
+        }
     }
 
     install() {
-        this.log(chalk.green("Setting up poetry project"));
         this.spawnCommandSync("poetry install -n");
-        this.spawnCommandSync("poetry run pre-commit install");
+        if (this.props.git) {
+            this.spawnCommandSync("poetry run pre-commit install");
+        }
     }
 };
